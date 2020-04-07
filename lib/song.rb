5173f1c8eb358bff731f17c0dc1a6d227f1cdc38
Tanner Townsend <rsmcwow@gmail.com>
2020-04-07 00:48:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,11 @@ class Song
   end
   
   def artist_name
+<<<<<<< HEAD
     if self.artist == nil
+=======
+    if self.arist == nil
+>>>>>>> 7d40472b38b0b4321146d537cefa9c5ba8a8787e
       return nil
     else
       @artist.name
